5586d8556693b0270c43b38bbddc2b1a3fcd09a6
Roger Espinosa <roger@umich.edu>
2022-12-12 20:57:23 -0500

    build and netlify

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].js',
+        assetFileNames: 'assets/[name].[ext]',
+      }
+    }
+  },
   plugins: [svelte({
     emitCss: false
   })],
